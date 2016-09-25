a3e152515c29e4b6099af9649dfcaf06aa9badc0
Michael James <michael@michaelbjames.com>
2016-09-24 17:42:33 -0700

    generalize location of links

---
+++ src/links.elm
@@ -0,0 +1,9 @@
+module Links exposing (..)
+
+github = "https://github.com/michaelbjames"
+resume = "resume"
+facebook = "https://facebook.com/michaeljames"
+flickr = "https://flickr.com/photos/91802135@N08/"
+linkedin = "https://linkedin.com/in/michael-b-james"
+
+
